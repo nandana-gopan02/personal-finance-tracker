7fb042f5c3eae6fcf38d65111d291228e08ed795
sainanda02 <nandanasreepillai@gmail.com>
2026-07-08 00:12:02 +0530

    Day 3 : Refactor finance tracker using functions

--- src/main.py
+++ src/main.py
@@ -1,17 +1,31 @@
-print("===== Personal Finance Tracker =====")
-print("1.Add Income")
-print("2.Add Expence")
-print("3.View Transaction")
-print("4.Exit")
 
+def show_menu():
+    print("===== Personal Finance Tracker =====")
+    print("1.Add Income")
+    print("2.Add Expence")
+    print("3.View Transaction")
+    print("4.Exit")
+
+def add_income():
+    print("Show income message")
+
+def add_expence():
+    print("Show expense message")
+
+def show_transaction():
+    print("Show transactions message")
+
+
+
+show_menu()
 choice=int(input("Enter a choice: "))
 
 if(choice ==1):
-    print("Show income message")
+    add_income()
 elif(choice==2):
-    print("Show expense message")
+    add_expence()
 elif(choice==3):
-    print("Show transactions message")
+    show_transaction()
 elif(choice==4):
     print("ThankYou, see you again")
 else:
