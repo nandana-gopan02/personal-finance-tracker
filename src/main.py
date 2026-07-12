8fba39348238b9ad50ed8928b9237ec2cba48276
sainanda02 <nandanasreepillai@gmail.com>
2026-07-12 21:14:13 +0530

    Added continuous menu

--- src/main.py
+++ src/main.py
@@ -1,32 +1,32 @@
 
 def show_menu():
-    print("===== Personal Finance Tracker =====")
+    print("\n===== Personal Finance Tracker =====")
     print("1.Add Income")
-    print("2.Add Expence")
+    print("2.Add Expense")
     print("3.View Transaction")
     print("4.Exit")
 
 def add_income():
-    print("Show income message")
+    print("Income feature coming soon...")
 
 def add_expence():
-    print("Show expense message")
+    print("Expense feature coming soon...")
 
-def show_transaction():
+def view_transaction():
     print("Show transactions message")
 
 
-
-show_menu()
-choice=int(input("Enter a choice: "))
-
-if(choice ==1):
-    add_income()
-elif(choice==2):
-    add_expence()
-elif(choice==3):
-    show_transaction()
-elif(choice==4):
-    print("ThankYou, see you again")
-else:
-    print("Invalid choice")
+while True:
+    show_menu()
+    choice=int(input("Enter a choice: "))
+    if(choice ==1):
+     add_income()
+    elif(choice==2):
+         add_expence()
+    elif(choice==3):
+        view_transaction()
+    elif(choice==4):
+        print("ThankYou, see you again")
+        break
+    else:
+        print("Invalid choice")
