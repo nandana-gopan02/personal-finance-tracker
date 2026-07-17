e1bb3e785e8eefbb1b64a921fb719da023dfa069
sainanda02 <nandanasreepillai@gmail.com>
2026-07-17 21:26:01 +0530

    Refactor transactions using dictionaries

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-transaction=[]
+transactions=[]
 def show_menu():
     print("\n===== Personal Finance Tracker =====")
     print("1.Add Income")
@@ -7,24 +7,45 @@ def show_menu():
     print("4.Exit")
 
 def add_income():
-   income=int(input("Enter Income Amount:"))
-   income_store= f"Income : {income}"
-   transaction.append(income_store)
+    income=int(input("Enter Income Amount:"))
+
+    if(income<0):
+       print("Amount cannot be negative.")
+    else:
+        transaction={
+            "Type":"Income",
+            "Amount":income
+        }
+        transactions.append(transaction)
+#    income_store= f"Type: Income ,Income : {income}"
+#    transaction.append(income_store)
 
 def add_expence():
-    expense=int(input("Enter Expense Amount: "))
-    expense_store = f"Expense: {expense}"
-    transaction.append(expense_store)
+    expence=int(input("Enter Expense Amount: "))
+    if(expence<0):
+        print("Amount cannot be negative.")
+    else:
+        transaction={
+            "Type":"Expence",
+            "Amount":expence
+        }
+        transactions.append(transaction)
+    # expense_store = f"Expense: {expense}"
+    # transaction.append(expense_store)
 
 def view_transaction():
-    if len(transaction)==0:
+    if len(transactions)==0:
         print("No transaction found")
     else:
         print("\nTransaction")
-        print("============")
+       
     
-    for i in transaction:
-        print(i)
+    for transaction in transactions:
+        print("-------------------")
+        print("\nType: ",transaction["Type"])
+        print("\nAmount: ",transaction["Amount"])
+    print("-----------------")
+      
 
 
 
